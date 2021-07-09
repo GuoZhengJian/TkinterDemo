dd856f3f6cbac0d56c3a48f2b91438ca42e5d4cb
GuoZhengJian <572078547@qq.com>
2021-07-09 15:52:33 +0800

    up

---
+++ 小技巧/交互式编程/Topwindow.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@author: GuoZhengJian
+
+E-mail : 572078547@QQ.COM
+"""
+
+import tkinter as tk
+        
+class Top(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__()
+        self.config(background='#AD4A4A', height=200)
+        self.pack(fill='both')
+               
